adf181b83c3816b692a5a6b121a83499299e62f2
Renan Monteiro <monteiro-renan@hotmail.com>
2023-05-08 15:49:55 -0300

    add communication with rabbitmq

---
+++ estoquepreco/src/main/java/com/renan/estoquepreco/constantes/RabbitMQConstantes.java
@@ -0,0 +1,6 @@
+package com.renan.estoquepreco.constantes;
+
+public class RabbitMQConstantes {
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+}
